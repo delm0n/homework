063102efa1e44264f81823b302d0c89692294f9a
delm0n <78958945+delm0n@users.noreply.github.com>
2021-03-09 01:26:22 +0300

    Лабораторная 2

---
+++ textfile/textfile/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace textfile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(502, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(250, 360);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Сброс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(323, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Найти первое";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(323, 247);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Найти второе";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(323, 290);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Поменять";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(340, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(31, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 360);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(340, 151);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Текстовые файлы, символы и строки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ textfile/textfile/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button4.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //создание контекстного меню
+            System.Windows.Forms.ContextMenu contextMenu1;
+            contextMenu1 = new System.Windows.Forms.ContextMenu();
+            System.Windows.Forms.MenuItem menuItem1;
+            menuItem1 = new System.Windows.Forms.MenuItem();
+            System.Windows.Forms.MenuItem menuItem2;
+            menuItem2 = new System.Windows.Forms.MenuItem();
+            System.Windows.Forms.MenuItem menuItem3;
+            menuItem3 = new System.Windows.Forms.MenuItem();
+            contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+menuItem1, menuItem2, menuItem3 });
+            menuItem1.Index = 0;
+            menuItem1.Text = "Открыть";
+            menuItem2.Index = 1;
+            menuItem2.Text = "Сохранить";
+            menuItem3.Index = 2;
+            menuItem3.Text = "Сохранить как";
+            richTextBox1.ContextMenu = contextMenu1;
+            menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+        }
+        string MyFName = "";
+
+        private void menuItem1_Click(object sender, System.EventArgs e)
+        {
+            openFileDialog1.Filter = "Текстовые файлы (*.rtf; *.txt; *.dat) | *.rtf;*.txt; *.dat";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MyFName = openFileDialog1.FileName;
+                richTextBox1.LoadFile(MyFName);
+            }
+
+        }
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            if (MyFName != "")
+            {
+                richTextBox1.SaveFile(MyFName);
+            }
+            else
+            {
+                saveFileDialog1.Filter = "Текстовые файлы (*.rtf; *.txt; *.dat) | *.rtf;*.txt; *.dat";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    MyFName = saveFileDialog1.FileName;
+                    richTextBox1.SaveFile(MyFName);
+                }
+            }
+        }
+        private void menuItem3_Click(object sender, System.EventArgs e)
+        {
+            saveFileDialog1.Filter = "Текстовые файлы (*.rtf; *.txt; *.dat) | *.rtf; *.txt; *.dat";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MyFName = saveFileDialog1.FileName;
+                richTextBox1.SaveFile(MyFName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            richTextBox1.Clear();
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+        }
+        int result1, result2;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int LenText;
+            textBox2.Text += "Поиск первого слова" + Environment.NewLine;
+            String FWord = textBox3.Text.ToString();
+            LenText = richTextBox1.Text.Length;
+            result1 = FindWord(FWord, LenText);
+            if (result1 != -1)
+            {
+                textBox2.Text += "Позиция первого слова: " + (result1 + 1) +
+               Environment.NewLine + Environment.NewLine;
+                richTextBox1.SelectionStart = result1;
+                richTextBox1.SelectionLength = FWord.Length;
+                richTextBox1.SelectionBackColor = Color.Red;
+                button2.Enabled = false;
+                if (button3.Enabled == false)
+                { button4.Enabled = true; }
+            }
+            else
+            {
+                textBox2.Text += "Слово не найдено " + Environment.NewLine
+               + Environment.NewLine;
+            }
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int LenText;
+
+            textBox2.Text += "Поиск второго слова" + Environment.NewLine;
+            String FWord = textBox1.Text.ToString();
+            LenText = richTextBox1.Text.Length;
+            result2 = FindWord(FWord, LenText);
+            if (result2 != -1)
+            {
+                textBox2.Text += "Позиция второго слова: " + (result2 + 1) +
+               Environment.NewLine + Environment.NewLine;
+                
+            richTextBox1.SelectionStart = result2;
+                richTextBox1.SelectionLength = FWord.Length;
+                richTextBox1.SelectionBackColor = Color.Green;
+                button3.Enabled = false;
+                if (button2.Enabled == false)
+                { button4.Enabled = true; }
+            }
+            else
+            {
+                textBox2.Text += "Слово не найдено " + Environment.NewLine +
+               Environment.NewLine;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (result1 < result2)
+            {
+                richTextBox1.Select(result2, textBox1.Text.Length);
+                richTextBox1.SelectedText = textBox3.Text.ToString();
+                richTextBox1.Select(result1, textBox3.Text.Length);
+                richTextBox1.SelectedText = textBox1.Text.ToString();
+                textBox2.Text += "Произошла замена слов";
+                button4.Enabled = false;
+            }
+            else
+            {
+                richTextBox1.Select(result1, textBox3.Text.Length);
+                richTextBox1.SelectedText = textBox1.Text.ToString();
+                richTextBox1.Select(result2, textBox1.Text.Length);
+                richTextBox1.SelectedText = textBox3.Text.ToString();
+                textBox2.Text += "Произошла замена слов";
+                button4.Enabled = false;
+            }
+        }
+
+
+        //изменение
+
+
+
+        private void Main(object sender, EventArgs e)
+        {
+            string Text = richTextBox1.Text.ToString();
+            string[] words = Text.Split(new char[] {' ', '\n' });
+            //string[] k;
+            List<string> k = new List<string>() {""};
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].Trim(new Char[] { ' ', ',', '!', '.','?',':',';', ',', '.', ';'});
+            }
+            int min = int.MaxValue;
+            int exmin=0;
+            foreach (String word in words)
+            {
+                if ((word.Length < min) && (word.Length != 0))
+                {
+                    k.Clear();
+                    min = word.Length;
+                    k.Add(word);
+                }
+                else if ((word.Length == min) && (word.Length != 0)) k.Add(word);
+            }
+            
+            int[] res = new int[(k.Count)];
+            string[] str = new string[(k.Count)];
+            int LongText = richTextBox1.Text.Length;
+            
+            for (int i = 0; i < k.Count; i++)
+            {
+                str[i] = k[i];
+                res[i] = FindWord(str[i], LongText);
+            } 
+            for (int i = 0; i < k.Count; i++)
+            {
+                if (res[i] != -1)
+                {
+
+                    richTextBox1.SelectionStart = res[i];
+                    richTextBox1.SelectionLength = min;
+                    richTextBox1.SelectionBackColor = Color.Gray;
+                    
+                }
+            }
+            
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Enter) Main(0, null);
+
+        }
+   
+        //конец изменения
+
+        int FindWord(String FWord, int n)
+        {
+            int LenWord;
+            String ComparText;
+            LenWord = FWord.Length;
+            for (int i = 0; i <= n - LenWord; i++)
+            {
+                ComparText = richTextBox1.Text.Substring(i, LenWord);
+                if (ComparText == FWord)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
